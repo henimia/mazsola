f729887f84880f16c323c2339f2c16cd9bc56584
henimia <henimia@gmail.com>
2021-03-23 20:53:27 +0100

    Add mazsola

--- src/main/java/com/codecool/whattoprint/Challenge.java
+++ src/main/java/com/codecool/whattoprint/Challenge.java
@@ -2,6 +2,13 @@ package com.codecool.whattoprint;
 
 class Challenge {
     public static String decideAndSay(String[] words, int number) {
-        return words[0];
+        String word = "";
+        if (number % 3 == 0 && number % 7 == 0) {
+            word = word.concat(words[0]) + word.concat(words[1]);
+            return word;
+        }
+        if (number % 3 == 0) return words[0];
+        if (number % 7 == 0) return words[1];
+        return "";
     }
 }
